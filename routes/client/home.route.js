e2d8dba8af962c3c78e3e7b7c69b8512015df302
iam-dha <iamdha0706@gmail.com>
2025-04-06 09:24:14 +0700

    import controller into route

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const route = express.Router();
 
-route.get("/", async (req, res) => {
-    res.render("./client/pages/home/index.pug");
-})
+const controller = require("../../controllers/client/home.controller");
+
+route.get("/", controller.index);
 
 module.exports = route;
 
