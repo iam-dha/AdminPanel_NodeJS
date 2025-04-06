4ac084517833e5c60c409e82d7c673f7af927ce6
iam-dha <iamdha0706@gmail.com>
2025-04-06 09:47:50 +0700

    config pug folder

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = async (req, res) => {
-    res.render("./client/pages/home/index.pug");
+    res.render("./client/pages/home/index.pug", {
+        titlePage: "Trang Chu"
+    });
 }
