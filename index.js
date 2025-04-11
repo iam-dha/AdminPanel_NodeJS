c7b1f368d6b78fba4f261f4b8e0fd4ab0d505037
iam-dha <iamdha0706@gmail.com>
2025-04-11 16:47:04 +0700

    config admin layout

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const systemConfig = require("./config/system.js");
 
 require("dotenv").config()
 
@@ -15,6 +16,11 @@ const port = process.env.PORT;
 app.set('views', './views');
 app.set('view engine', 'pug'); // which template engine
 app.use(express.static("public"));
+
+// App local variables
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 
 // Routes
 routeClient(app);
