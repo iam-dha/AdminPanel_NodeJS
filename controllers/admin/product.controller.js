040a81f8e913860efdb97b34872f3324dacf6492
iam-dha <iamdha0706@gmail.com>
2025-04-11 17:35:59 +0700

    display data from db in admin-product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,17 @@
+const Product = require("../../models/product.model");
+
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+
+    const productList = await Product.find({
+        deleted: false
+    });
+    
+    console.log(productList)
+
     res.render("./admin/pages/products/index.pug", {
-        titlePage: "Trang san pham"
+        titlePage: "Trang san pham",
+        products: productList
     });
 }
