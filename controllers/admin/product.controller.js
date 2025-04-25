e6c3757d94596af82436a0bd21401ac4081f6841
iam-dha <iamdha0706@gmail.com>
2025-04-25 21:59:45 +0700

    add category when edit and create product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,9 +1,11 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
 const redirectBack = require("../../helpers/redirectBack");
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -141,9 +143,15 @@ module.exports.deleteItem = async (req, res) => {
 };
 
 // [GET] admin/products/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+    const categories = await ProductCategory.find(find);
+    const newCategories = createTreeHelper.create(categories);
     res.render("admin/pages/products/create", {
-        pageTitle: "Add new product"
+        pageTitle: "Add new product",
+        categories: newCategories
     });
 }
 
@@ -172,9 +180,14 @@ module.exports.edit = async (req, res) => {
     }
 
     const product = await Product.findOne(find);
+    const categories = await ProductCategory.find({
+        deleted: false
+    });
+    const newCategories = createTreeHelper.create(categories);
     res.render("admin/pages/products/edit", {
         pageTitle: "Edit product",
-        product: product
+        product: product, 
+        categories: newCategories
     });
 }
 
