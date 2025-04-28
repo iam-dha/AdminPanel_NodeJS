a5cc9a1fc7f777559facd009f77d982c13507576
iam-dha <iamdha0706@gmail.com>
2025-04-28 13:54:06 +0700

    complete edit role

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -22,11 +22,44 @@ module.exports.create = async (req, res) => {
         titlePage: "Add Role",
     });
 }
+
+// [POST] /admin/roles/create
 module.exports.createPost =  async (req, res) => {
     console.log(req.body);
     
     const newRole = new Role(req.body);
     await newRole.save();
-    
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
 }
+
+//[GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+    let find = {
+        _id: id,
+        deleted: false
+    };
+    try {
+        const role = await Role.findOne(find);
+        res.render("./admin/pages/roles/edit.pug",{
+            role: role
+        });
+
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`)
+    }
+}
+
+//[PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Role.updateOne({_id: id}, req.body);
+        req.flash("success", "Update Successfully");
+    } catch (error) {
+        req.flash("error", "Update Fail");
+    }
+    
+    redirectBack(req, res);
+}
+
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -8,7 +8,12 @@ const router = express.Router();
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
+
 router.post("/create", controller.createPost);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id", controller.editPatch);
 
 module.exports = router;
 
